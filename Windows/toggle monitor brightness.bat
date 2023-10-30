3eed2cb1c0e6c962285e2e52a7283afd4fe62f9d
jbao161 <jbao161@gmail.com>
2023-10-30 09:01:27 +0000

    Add files via upload

---
+++ Windows/toggle monitor brightness.bat
@@ -0,0 +1,3 @@
+%OneDrive%\Tools\controlmymonitor\ControlMyMonitor.exe /SwitchValue Primary 10 16 23 44
+Sleep 100
+%OneDrive%\Tools\controlmymonitor\ControlMyMonitor.exe /SwitchValue Primary 12 16 60 72
